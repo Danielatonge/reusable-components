0e675439595c543252b4b7ec8943855682db51a8
DanielAtonge <elamboatonge@gmail.com>
2021-12-29 04:01:14 +0300

    named slots

--- vuejs/rcomponents/src/App.vue
+++ vuejs/rcomponents/src/App.vue
@@ -1,19 +1,26 @@
 <template>
-  <AppButton @click="log">
-    <Icon />
-    CLICK
-  </AppButton>
+  <AppLayout>
+    <template #header> Slots are awesome </template>
+    <template #default>
+      <AppButton @click="log">
+        <Icon />
+        CLICK
+      </AppButton>
+    </template>
+  </AppLayout>
 </template>
 
 <script>
 import AppButton from "@/components/AppButton";
 import Icon from "@/components/Icon";
+import AppLayout from "@/components/AppLayout";
 
 export default {
   name: "App",
   components: {
     AppButton,
-    Icon
+    Icon,
+    AppLayout
   },
   methods: {
     log() {
---
+++ vuejs/rcomponents/src/components/AppLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header>
+      <slot name="header" />
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <slot name="footer">made with slots</slot>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
