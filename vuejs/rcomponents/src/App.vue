d84d5462380d818408d657c0cf7433b6da73b5cd
DanielAtonge <elamboatonge@gmail.com>
2021-12-29 03:30:09 +0300

    uses props

---
+++ vuejs/rcomponents/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <AppButton text="CLICK"></AppButton>
+</template>
+
+<script>
+import AppButton from "@/components/AppButton";
+
+export default {
+  name: "App",
+  components: {
+    AppButton
+  }
+};
+</script>
+
+<style></style>
