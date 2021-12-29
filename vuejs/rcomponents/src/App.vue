9cdd93f233ba5b57f7e4da69955b3161148e7b81
DanielAtonge <elamboatonge@gmail.com>
2021-12-29 05:44:23 +0300

    loading screen spinner

--- vuejs/rcomponents/src/App.vue
+++ vuejs/rcomponents/src/App.vue
@@ -2,25 +2,28 @@
   <AppLayout>
     <template #header> Slots are awesome </template>
     <template #default>
-      <AppButton @click="log">
-        <Icon />
-        CLICK
-      </AppButton>
+      <AppUserList>
+        <template #loading>
+          <AppSpinner> </AppSpinner>
+        </template>
+      </AppUserList>
     </template>
   </AppLayout>
 </template>
 
 <script>
-import AppButton from "@/components/AppButton";
-import Icon from "@/components/Icon";
+import AppSpinner from "@/components/AppSpinner";
+// import Icon from "@/components/Icon";
 import AppLayout from "@/components/AppLayout";
+import AppUserList from "@/components/AppUserList";
 
 export default {
   name: "App",
   components: {
-    AppButton,
-    Icon,
-    AppLayout
+    AppSpinner,
+    // Icon,
+    AppLayout,
+    AppUserList
   },
   methods: {
     log() {
